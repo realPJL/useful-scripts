347086315152cc7a46a8ba64b3c65c37e5e3fba2
PJL <pauljohannlorenz@proton.me>
2024-04-18 08:27:35 +0200

    Initial commit

---
+++ proofreading.py
@@ -0,0 +1,14 @@
+import lmproof
+
+def proofread(text):
+    proofread = lmproof.load("en")
+    correction = proofread.proofread(text)
+    print("Original: {}".format(text))
+    print("Correction: {}".format(correction))
+
+input = input("Enter text here: ")
+
+proofread(input)
+
+# TODO fix error
+# E050] Can't find model 'en_core_web_sm'. It doesn't seem to be a Python package or a valid path to a data directory.
