7ac3bbcdf7bf41b99a2822e1c8a625090a37ee8d
Paul Lorenz <pauljohannlorenz@proton.me>
2024-04-20 10:36:44 +0200

    Added logging feature

--- scan_network.py
+++ scan_network.py
@@ -3,11 +3,15 @@
 
 # pip install python-nmap
 import nmap
+import logging
+
+# Logging config
+logging.basicConfig(filename='log_scan_network.txt', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def scan_network():    
     scanner = nmap.PortScanner()
 
-    print("Scanning for live hosts...")
+    logging.info("Scanning for live hosts...")
     scanner.scan(hosts= target_network, arguments='-sn')    # -sn -> ping sweep without attempting to port scan the host (sends ICMP Echo Request ping)
 
     # List of all hosts that responded to ping
@@ -16,63 +20,75 @@ def scan_network():
     # Perform detailed port scan on each live host
     for host in live_hosts:
 
-        print(f"Scanning host {host}...")
+        logging.info(f"Scanning host {host}...")
         # -p    specifies range of ports to scan
         # -sV   perform version detection
         # -T4   sets timing template to aggressive
         # -O    operating system detection
         # -sC   script scanning
         scan_arguments = '-p 1-1023 -O -sV -sC -T4 --script vuln'
-        print(f"Scan command: nmap {host} {scan_arguments}")
+        logging.info(f"Scan command: nmap {host} {scan_arguments}")
         scanner.scan(hosts=host, arguments=scan_arguments)
 
 
         # Print scan results for current host
-        print(f"Results for host {host}: ")
+        logging.info(f"Results for host {host}: ")
         for host in scanner.all_hosts():
-            print(f"Host:   {host}")
+            logging.info(f"Host:   {host}")
 
             if 'osmatch' in scanner[host]:
-                print("Operating System:")
+                logging.info("Operating System:")
                 for os_match in scanner[host]["osmatch"]:
-                    print(f"- {os_match['name']} (Accuracy: {os_match['accuracy']})")
+                    logging.info(f"- {os_match['name']} (Accuracy: {os_match['accuracy']})")
             
             if 'scripts' in scanner[host]:
-                print("Script Scan Results:")
+                logging.info("Script Scan Results:")
                 for script_id, script_result in scanner[host]['scripts'].items():
-                    print(f"- {script_id}:  {script_result['output']}")
+                    logging.info(f"- {script_id}:  {script_result['output']}")
 
             for proto in scanner[host].all_protocols():
-                print(f"Protocol: {proto}")
+                logging.info(f"Protocol: {proto}")
                 ports = scanner[host][proto].keys()
 
                 for port in ports:
                     service = scanner[host][proto][port]['name']
                     state = scanner[host][proto][port]['state']
-                    print(f"Port: {port}\tState: {state}\tService: {service}")
+                    logging.info(f"Port: {port}\tState: {state}\tService: {service}")
 
                     if 'product' in scanner[host][proto][port]:
                         product = scanner[host][proto][port]['product']
-                        print(f"Service Banner: {product}")
+                        logging.info(f"Service Banner: {product}")
 
                     # Check if vulnerability scan results are available for the port
                     if 'script' in scanner[host][proto][port]:
                         vulnerability_scripts = scanner[host][proto][port]['script']
 
                         if 'vulners' in vulnerability_scripts:
-                            print("Vulnerability Scan Results:")
+                            logging.info("Vulnerability Scan Results:")
 
                             vuln_result = ""
 
                             for vulnerability_script in vulnerability_scripts['vulners']:
                                 vuln_result += vulnerability_script
-                            print(vuln_result)
+                            logging.info(vuln_result)
                             
                         else:
-                            print("No vulnerability scan results available.")
+                            logging.info("No vulnerability scan results available.")
                     else:
-                            print("No vulnerability scan results available.")
+                        logging.info("No vulnerability scan results available.")
+
+def print_results_to_console():
+    if print_results == 'y':
+        with open('log_scan_network.txt', 'r') as f:
+            file_content = f.read()
+        print(file_content)
+    elif print_results == 'n':
+        print('The script closes now. You can see the results in the log_scan_network.txt file.')
+
 
 if __name__ == "__main__":
     target_network = input("Enter the target network or IP range to scan: ")        # localhost / loopback = 127.0.0.0/8
     scan_network()
+    print("Scan results saved in log_scan_network.txt")
+    print_results = input("Do you want to print them out here? (y,n): ")
+    print_results_to_console()
